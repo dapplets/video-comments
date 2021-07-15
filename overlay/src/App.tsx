048026be6040dde9a2f700dd8d36aaff10230e7a
Nikita Teremovskiy <ni-2@mail.ru>
2021-07-15 18:04:16 +0300

    feat: add comments template to overlay.

--- overlay/src/App.tsx
+++ overlay/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu, MenuItemProps } from 'semantic-ui-react';
 import { bridge } from './dappletBridge';
+import Comments from './Comments';
 
 interface Props {}
 
@@ -21,7 +22,30 @@ export default class App extends React.Component<Props, State> {
   }
 
   componentDidMount() {
-    bridge.onData((data) => this.setState({ ...defaultState, ...data }));
+    bridge.onData((data) => this.setState({ ...defaultState, ...data }, async () => {
+      try {
+        const res = await fetch('https://comments.dapplets.org/auth/anonymous/login?user=0x692a4d7B7BE2dc1623155E90B197a82D114a74f3&site=remark&aud=remark');
+        const token = res.headers.get('X-Jwt');
+        const headers: HeadersInit = new Headers();
+        if (token) {
+          headers.set('X-Jwt', token!);
+          await fetch('https://comments.dapplets.org/api/v1/user', { headers }).then(x => x.json()).then((a) => console.log('aaaaaaaaaaaaa', a));
+        }
+      } catch (e) {
+        console.log(e);
+      } 
+      try {
+        const res = await fetch('https://comments.dapplets.org/auth/anonymous/login?user=0x9126d36880905fcb9e5f2a7f7c4f19703d52bc62&site=remark&aud=remark');
+        const token = res.headers.get('X-Jwt');
+        const headers: HeadersInit = new Headers();
+        if (token) {
+          headers.set('X-Jwt', token!);
+          await fetch('https://comments.dapplets.org/api/v1/user', { headers }).then(x => x.json()).then((a) => console.log('bbbbbbbbbbbb', a));
+        }
+      } catch (e) {
+        console.log(e);
+      } 
+    }));
   }
 
   handleItemClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, data: MenuItemProps ) => this.setState({ activeItem: data.name! })
@@ -37,37 +61,40 @@ export default class App extends React.Component<Props, State> {
       lineHeight: '100%',
     };
     return (
-      <Menu tabular>
-        <Menu.Item
-          name=''
-          style={itemStyle}
-          active={activeItem === ''}
-        />
-        <Menu.Item
-          name='All'
-          style={itemStyle}
-          active={activeItem === 'All'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Active'
-          style={itemStyle}
-          active={activeItem === 'Active'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Inactive'
-          style={itemStyle}
-          active={activeItem === 'Inactive'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Hidden'
-          style={itemStyle}
-          active={activeItem === 'Hidden'}
-          onClick={this.handleItemClick}
-        />
-      </Menu>
+      <>
+        <Menu tabular>
+          <Menu.Item
+            name=''
+            style={itemStyle}
+            active={activeItem === ''}
+          />
+          <Menu.Item
+            name='All'
+            style={itemStyle}
+            active={activeItem === 'All'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Active'
+            style={itemStyle}
+            active={activeItem === 'Active'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Inactive'
+            style={itemStyle}
+            active={activeItem === 'Inactive'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Hidden'
+            style={itemStyle}
+            active={activeItem === 'Hidden'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+        <Comments item={this.state.activeItem}/>
+      </>
     );
   }
 }
--- overlay/src/Comments.tsx
+++ overlay/src/Comments.tsx
@@ -1,1 +1,179 @@
-export default () => {}
+import React, { useState } from 'react';
+import { Icon, Container, Card, Comment } from 'semantic-ui-react';
+
+interface IData {
+  id: number,
+  name: string,
+  time: string,
+  text: string,
+  image?: string,
+  from: number,
+  to: number,
+}
+
+const mockedData: IData[] = [
+  {
+    id: 1,
+    name: 'Matt',
+    time: 'Today at 5:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    image: 'https://react.semantic-ui.com/images/avatar/small/matt.jpg',
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 2,
+    name: 'Olivia',
+    time: 'Today at 6:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    image: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 3,
+    name: 'Вася',
+    time: 'Today at 7:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 4,
+    name: 'Петя',
+    time: 'Today at 8:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 5,
+    name: 'Matt',
+    time: 'Today at 5:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    image: 'https://react.semantic-ui.com/images/avatar/small/matt.jpg',
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 6,
+    name: 'Olivia',
+    time: 'Today at 6:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    image: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 7,
+    name: 'Вася',
+    time: 'Today at 7:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    from: 0,
+    to: Infinity,
+  },
+  {
+    id: 8,
+    name: 'Петя',
+    time: 'Today at 8:42PM',
+    text: `In our quest to improve the user experience, 
+    we miss that those seeking to displace traditional production, 
+    nanotechnology, which is a vivid example of a continental-European
+    type of political culture, will be objectively considered 
+    by the relevant authorities!`,
+    from: 0,
+    to: Infinity,
+  },
+];
+
+interface IProps {
+  item: string,
+}
+
+export default function Comments(props: IProps) {
+  // Declare a new state variable, which we'll call "count"
+
+  return (
+    <Container className='overlay-card'>
+      {mockedData.map((data) => comment(data))}
+    </Container>
+  );
+}
+
+function comment(data: IData) {
+  const { id, name, time, text, image } = data;
+  const [isCollapsed, toggleIsCollapsed] = useState(true);
+  const [isHidden, toggleIsHidden] = useState(false);
+  const handleToggleHidden = () => {
+    toggleIsHidden(!isHidden)
+  }
+  return (
+    <Card style={{ width: '100%', minHeight: '62px' }} className={isHidden ? 'comment-hidden' : ''}>
+      <Card.Content>
+        <Comment.Group>
+          <Comment>
+            <Comment.Avatar src={image} />
+            {!image && (
+              <div className='default-badge'>
+                {name[0].toUpperCase()}
+              </div>)}
+            <Comment.Content>
+              <div className='comment-header'>
+                <Comment.Author as='a'className={isHidden ? 'comment-hidden' : ''}>
+                  {name}
+                </Comment.Author>
+                <Comment.Metadata style={{ flexGrow: 1 }}>
+                  <div>
+                    {time}
+                  </div>
+                </Comment.Metadata>
+                <Icon name='eye' className='eye-icon' onClick={handleToggleHidden} />
+              </div>
+              <Comment.Text hidden={isHidden}>
+                {text.length > 103 && isCollapsed
+                  ? `${text.substring(0, 99)}...`
+                  : text}
+              </Comment.Text>
+              <Comment.Actions hidden={isHidden}>
+                <Comment.Action
+                  index={id}
+                  onClick={() => toggleIsCollapsed(!isCollapsed)}
+                >
+                  {isCollapsed ? 'More' : 'Less'}
+                </Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        </Comment.Group>
+      </Card.Content>
+    </Card>
+  )
+}
